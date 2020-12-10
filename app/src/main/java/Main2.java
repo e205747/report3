4d7e3913d85ad7d0b77b4e5e86e2e6c6574af82f
Moe ASATO <e205747@ie.u-ryukyu.ac.jp>
2020-12-11 03:53:43 +0900

    report4 4th

--- app/src/main/java/Main2.java
+++ app/src/main/java/Main2.java
@@ -2,8 +2,9 @@ import jp.ac.uryukyu.ie.e205747.*;
 
 public class Main2 {
     public static void main(String[] args){
-        Hero hero = new Warrior("勇者", 10, 5); // (a)
+        Hero hero = new Hero("勇者", 10, 5); // (a)
         Enemy enemy = new Enemy("スライム", 6, 3);
+        System.out.println(hero);
 
         System.out.printf("%s vs. %s\n", hero.getName(), enemy.getName());
 
@@ -11,7 +12,7 @@ public class Main2 {
         while( hero.isDead() == false && enemy.isDead() == false ){
             turn++;
             System.out.printf("%dターン目開始！\n", turn);
-            hero.attack(enemy); // (b)
+            hero.attack(enemy);  // (b)
             enemy.attack(hero);
         }
         System.out.println("戦闘終了");
